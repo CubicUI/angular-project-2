8e21c329025fd793cf54420bd33a9d048446d875
CubicUI <ui@cubicit.net>
2017-03-21 11:13:26 -0500

    fixed comment tag

--- sqlscripts.sql
+++ sqlscripts.sql
@@ -1,8 +1,8 @@
---drops schema if it exists
+#drops schema if it exists
 DROP SCHEMA `attendance`;
 CREATE SCHEMA `attendance` ;
 
---creating class table
+#creating class table
 CREATE TABLE `attendance`.`classtbl` (
   `classid` INT NOT NULL AUTO_INCREMENT,
   `title` VARCHAR(45) NOT NULL,
@@ -10,21 +10,21 @@ CREATE TABLE `attendance`.`classtbl` (
   PRIMARY KEY (`classid`),
   UNIQUE INDEX `classid_UNIQUE` (`classid` ASC));
 
---inserting information for classtable
+#inserting information for classtable
 INSERT INTO `attendance`.`classtbl` (`title`, `active`) VALUES ('Java', '1');
 INSERT INTO `attendance`.`classtbl` (`title`, `active`) VALUES ('.NET', '1');
 INSERT INTO `attendance`.`classtbl` (`title`, `active`) VALUES ('VMWare', '1');
 INSERT INTO `attendance`.`classtbl` (`title`, `active`) VALUES ('Angular', '1');
 INSERT INTO `attendance`.`classtbl` (`title`, `active`) VALUES ('linux', '0');
 
---creating attendance table
+#creating attendance table
 CREATE TABLE `attendance`.`workstatus` (
   `workstatusid` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `title` VARCHAR(45) NOT NULL,
   `active` INT NOT NULL,
   PRIMARY KEY (`workstatusid`));
   
---inserting information table
+#inserting information table
 INSERT INTO `attendance`.`workstatus` (`title`, `active`) VALUES ('US Citizen', '1');
 INSERT INTO `attendance`.`workstatus` (`title`, `active`) VALUES ('Green Card', '1');
 INSERT INTO `attendance`.`workstatus` (`title`, `active`) VALUES ('EAD (GC)', '1');
